d20ca0ce3798872c90ca816cbf8b184498e2d1ae
pavelegorenkov <epipasha@gmail.com>
2020-11-15 16:43:44 +0300

    Add detail movie screen

--- app/src/main/java/ru/epipasha/academy/MainActivity.kt
+++ app/src/main/java/ru/epipasha/academy/MainActivity.kt
@@ -1,11 +1,23 @@
 package ru.epipasha.academy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pushButton : Button = findViewById<Button>(R.id.btn_push)
+        pushButton.setOnClickListener {
+            moveToDetailScreen()
+        }
+    }
+
+    private fun moveToDetailScreen(){
+        val intent = Intent(this, MovieDetailsActivity::class.java)
+        startActivity(intent)
     }
 }
